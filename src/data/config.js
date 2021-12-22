718d164c8d3dc9b0e7bd8c4f0bc9125753b6bf07
Sēlekkt <selekkt@gmail.com>
2021-12-23 00:54:14 +0100

    Update config.js

--- src/data/config.js
+++ src/data/config.js
@@ -1,11 +1,10 @@
 export default {
-    siteName: `Selekkt. studio`,
+    name: `Selekkt. studio`,
     title: `Skelet.css`,
     description: `Skelet.css on Astro.`,
     url: `https://selekkt.dk/skelet/v3`, // No trailing slash!
     themeColor: `#FFFFFF`,
-    // JSON LD
-    name: `Example`,
+
     facebookURL: `https://facebook.com`,
     twitterURL: `https://twitter.com/selekktstudio`,
     instagramURL: `https://instagram.com/selekkt.studio`,
